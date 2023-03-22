4ce68c81b31aa15d19a1660c301e22aa8a1974d4
Neil_patel_Jai Shree Krishna <neilpatel235@gmail.com>
2023-03-22 14:25:01 -0400

    Neil added the decoder() function that works properly to Kian's code for lab 6.

--- Lab06 Test.py
+++ Lab06 Test.py
@@ -1,20 +1,41 @@
 #Kian Ambrose
-
-
-def encode(string):
+# Neil Patel is making this edit
+def encoder(string):
     d = []
     for i in string:
-        a= int(i)
-        if a<7:
-            b = a+3
+        a = int(i)
+        if a < 7:
+            b = a + 3
         else:
-            b = a-7
-        b= str(b)
+            b = a - 7
+        b = str(b)
         d.append(b)
-    str1=""
+    str1 = ""
     for j in d:
         str1 += j
     return str1
+
+
+def decoder(fake_p):
+    fake_p = list(fake_p)
+    string2 = ""
+    for i in range(len(fake_p)):
+        if fake_p[i] == "0":
+            fake_p[i] = "7"
+            string2 += fake_p[i]
+            continue
+        if fake_p[i] == "2":
+            fake_p[i] = "9"
+            string2 += fake_p[i]
+            continue
+        if fake_p[i] == "1":
+            fake_p[i] = "8"
+            string2 += fake_p[i]
+            continue
+        if fake_p[i] == "3" or fake_p[i] == "4" or fake_p[i] == "5" or fake_p[i] == "6" or fake_p[i] == "7" or fake_p[i] == "8" or fake_p[i] == "9":
+            real = (int(fake_p[i]) - 3)
+            string2 += str(real)
+    return string2
 
 
 if __name__ == "__main__":
@@ -25,24 +46,13 @@ if __name__ == "__main__":
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
-        x = input("Please enter an option: ")
+        x = input("\nPlease enter an option: ")
 
-        c = input("Please enter your password to encode: ")
-
-
-
-
-
-        if x== "1":
-            encode(c)
-
-            print("Your password has been encoded and stored!")
-
+        if x == "1":
+            c = input("Please enter your password to encode: ")
+            store_fake = encoder(c)
+            print("Your password has been encoded and stored!\n")
         elif x == "2":
-            pass
-
+            print(f"The encoded password is {store_fake}, and the original password is {decoder(store_fake)}.\n")
         elif x == "3":
             break
-
-
-
